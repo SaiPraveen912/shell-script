00fd10868ae937b2177e01b43487a037033670d1
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-19 12:44:42 +0530

    special-variables-user

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -14,3 +14,6 @@ echo "Current working dierectory: $PWD"
 
 # to get the home dierectory of a user to know who is running the script -> $HOME
 echo "Home dierectory of current user: $HOME"
+
+# to know which user is running
+echo "Which user is running this script: $USER"
