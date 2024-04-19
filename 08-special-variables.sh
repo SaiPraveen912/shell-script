bd6406256548f9701bad7755cecb512217b50dd0
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-19 12:46:52 +0530

    special-variables-hostname

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -15,5 +15,9 @@ echo "Current working dierectory: $PWD"
 # to get the home dierectory of a user to know who is running the script -> $HOME
 echo "Home dierectory of current user: $HOME"
 
-# to know which user is running
+# to know which user is running -> $USER
 echo "Which user is running this script: $USER"
+
+# to know the host name -> $HOSTNAME
+echo "Hostname: $HOSTNAME"
+
