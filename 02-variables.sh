5b6c87c7f0461a6eb407f3c97f2f6d48cb72de74
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-19 11:21:02 +0530

    variables-conversation

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,4 +4,5 @@ echo "Ramesh:: Hello Suresh, How are you?"
 echo "Suresh:: Hello Ramesh, I am fine. How are you?"
 echo "Ramesh:: I am fine, how is your work?"
 echo "Suresh:: not bad, I am thinking to upgrade to DevOps"
+echo "Ramesh:: that' fine, thank you take care."
 
