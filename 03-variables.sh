a36b8744ba02be2d8f20d841bf2323b7125f105d
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-19 12:13:59 +0530

    variables-read-arguments

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,7 +6,7 @@ PERSON2=Rahul
 
 #refferring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "${PERSON2}:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine, how is your work?"
 echo "$PERSON2:: not bad, I am thinking to upgrade to DevOps"
 echo "$PERSON1:: that' fine, thank you take care."
---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# declaring array
+MOVIES=("RRR" "DjTillu" "Devara") # list of values
+
+# list always starts with 0.
+# size of above array 3
+# indexes are 0,1,2 index = size-1
+
+echo "First Movie is :"
