ac24f48e76e32c38c863bbf50d367de3ae39d4ee
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-19 12:40:02 +0530

    special-variables-PWD

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,8 +3,11 @@
 # to print every variables given to script externally -> $@
 echo "All variables: $@"
 
-# number of variables passed -> $#
+# number of variables passed in number -> $#
 echo "No of variables passed: $#"
 
 # to know the script name -> $0
 echo "Scrpt name: $0"
+
+# to know the present working dierectory -> $PWD
+echo "Current working dierectory: $PWD"
