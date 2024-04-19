aa2732b78f6157c52d45a48c9ffee59b326801ca
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-19 11:42:20 +0530

    variables-ramesh-suresh

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh #no space between = and value 
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine, how is your work?"
+echo "$PERSON2:: not bad, I am thinking to upgrade to DevOps"
+echo "$PERSON1:: that' fine, thank you take care."
