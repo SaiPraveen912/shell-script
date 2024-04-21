57d17ed808b8259639a28dc7127749bdaee1f136
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-21 11:51:27 +0530

    reinstallation

--- 10-installation.sh
+++ 10-installation.sh
@@ -38,6 +38,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ] # If 0 then super/root user otherwise normal user
 then    
     echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
     echo "You are super user."
 fi    
