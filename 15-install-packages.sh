f77a21ab868d070add4554f0ffa32f51274ba57f
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-21 13:56:18 +0530

    install-packages

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -13,6 +13,22 @@
 
 # 1. user have root access or not
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S) # 2024-04-21-07-00-14 -> Which time this is getting executed
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # $0 -> to get the script name
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ] # $1 have exit status of cmd : dnf install mysql -y
+    then
+        echo -e "$2...$R FAILURE $N" # -e for enabling colors $R for red $N for normal
+        exit 1 # if FAILURE then only exit ortherwise no need to exit
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi    
+}
+
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with super user"
@@ -28,7 +44,14 @@ fi
 # echo "All packages: $@" # ->> All packages: mysql git gcc fail2ban
 for i in $@
 do 
-    echo "package to install: $i"
+    echo "package to install: $i" # $i contains these values : mysql git gcc fail2ban
+    dnf list installed $i &>>$LOGFILE # checks if these are installed : mysql git gcc fail2ban and redierects to $LOGFILE
+    if [ $? -eq 0 ] # if exit status is 0 if installed
+    then
+        echo "$i already installed...SKIPPING"
+    else
+        echo "$i not installed...Need to install"
+    fi
 done
  
 
