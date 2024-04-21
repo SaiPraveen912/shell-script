afd9900d64ff9d6dfae40d616c6fe1d291dacba2
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-21 12:17:13 +0530

    re-functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
+VALIDATE(){
+    # $? -> $1 and "Installing MySQL" -> $2
+    echo "Exit Status: $1"
+    echo "What are you doing: $2"
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with super user"
-    exit 1
+    exit 1 # manually exit if error comes
 else
     echo "You are super user"
 fi
 
+
+
 dnf install mysql -y
-if [ $? -ne 0 ]
-then 
-    echo "Installation of MySQL...FAILURE"
-else
-    echo "Installation of MySQL...SUCCESS"
-fi 
+# $? -> $1 and "Installing MySQL" -> $2
+VALIDATE $? "Installing MySQL" # Passing 2 argument to function
+ 
 
 
 dnf install git -y
-if [ $? -ne 0 ]
-then 
-    echo "Installation of Git...FAILURE"
-else
-    echo "Installation of Git...SUCCESS"
-fi 
+# $? -> $1 and "Installing MySQL" -> $2
+VALIDATE $? "Installing Git" # Passing 2 argument to function
 
-echo "Is script proceeding?"
