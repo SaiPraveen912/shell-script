bc833f47786370cd0b59f06509cf91fea76bec7e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-21 13:40:34 +0530

    install-packages

---
+++ 15-install-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# try to install these packages 
+# mysql
+# gcc
+# chrony
+# fail2ban
+# postgis2_94
+
+# steps
+# log redierection
+# colors are mandatory
+
+# 1. user have root access or not
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with super user"
+    exit 1 # manually exit if error comes
+else
+    echo "You are super user"
+fi
+
+# send packages to install as parameters
+# sudo sh install-packages mysql gcc docker
+# special variable to get all params --> $@
+
+echo "All packages: $@"
+
