43a5110026a817cf505362dc15e6f1ae5fb0597e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-04-25 13:04:15 +0530

    delete-log-files

---
+++ 19-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE_DIERECTORY=/tmp/app-logs
+
+R="\e[31m" # red color symbol
+G="\e[32m" # green color symbol
+Y="\e[33m"
+N="\e[0m"  # normal color symbol
+
+if [ -d $SOURCE_DIERECTORY ]
+then
+    echo -e "$G Source dierectory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIERECTORY exists $N"
+    exit 1
+fi
